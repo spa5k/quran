ca7f4d962ce8ea2961efd69ba495ff3c6f50da66
Abdullah Ahmed <52929221+BasiOfficial@users.noreply.github.com>
2023-05-19 17:59:25 -0500

    yep

--- common/core/src/layout/DailyHadith.tsx
+++ common/core/src/layout/DailyHadith.tsx
@@ -1,37 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 export default function DailyAyat() {
-  const [ayah, setAyah] = useState('');
-  const [translation, setTranslation] = useState('');
+  const [hadith, setHadith] = useState('');
 
   useEffect(() => {
-    const fetchRandomAyah = async () => {
-      const response = await fetch('https://api.alquran.cloud/v1/ayah/random');
-      const data = await response.json();
+    const fetchRandomHadith= async () => {
+      const hadithCollection = await fetch('https://api.sunnah.com/v1/hadiths/random');
+      const data = await hadithCollection.json();
 
-      const ayahText = `${data.data.text} (${data.data.surah.name}, Ayah ${data.data.numberInSurah})`;
-      setAyah(ayahText);
-
-      const translationResponse = await fetch(`https://api.alquran.cloud/v1/ayah/${data.data.surah.number}:${data.data.numberInSurah}/en.asad`);
-      const translationData = await translationResponse.json();
-
-      setTranslation(translationData.data.text);
-      
+      const hadithText = `${hadith[]}`;
+      setHadith(hadithText);
     };
-    fetchRandomAyah();
-    const intervalId = setInterval(fetchRandomAyah, 24 * 60 * 60 * 1000);
+    fetchRandomHadith();
+    const intervalId = setInterval(fetchRandomHadith, 24 * 60 * 60 * 1000);
 
     return () => {
       clearInterval(intervalId);
     };
-
+    
   }, []);
 
   return (
     <div style={{textAlign: "center", border: "0px solid", borderRadius: '10px', background: '#dedede', backgroundClip: 'padding-box', width: '250px', height: '210px', margin: '0 auto', marginTop: "20px", color: "black"}}>
       <p><strong>Daily Hadith</strong></p>
-      <p>{ayah}</p>
-      <p style={{ fontSize: '10px' }}><br></br>{translation}</p>
+      <p>{hadith}</p>
     </div>
   );
 }
