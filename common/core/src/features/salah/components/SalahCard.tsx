83ff287e79c49f06f475e9a8b5b5089be876afa6
Abdullah Ahmed <52929221+BasiOfficial@users.noreply.github.com>
2023-05-23 09:02:08 -0500

    theme update

--- common/core/src/features/salah/components/SalahCard.tsx
+++ common/core/src/features/salah/components/SalahCard.tsx
@@ -1,7 +1,7 @@
 export const SalahCard = ({ prayer, time }: { prayer: string; time: Date; }) => {
 	return (
-		<div className='mx-auto' key={prayer}>
-			<div className='m-3 flex  w-40  items-center justify-center rounded-lg py-3 font-bold  shadow-md'>
+		<div className='mx-auto' key={prayer} style={{color: 'white'}}>
+			<div className='m-3 flex  w-40  items-center justify-center rounded-lg py-3 font-bold  shadow-md' style={{backgroundColor: '#16223d'}}>
 				<p className='mx-2 font-bold'>{prayer} :</p>
 				{time.toLocaleString('en-US', { hour: 'numeric', hour12: true, minute: '2-digit' })}
 			</div>
